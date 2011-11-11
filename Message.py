35d3fd77f64782e8837eed36bc00fc7baf72da79
Krzysztof Medrela <krzysiumed@gmail.com>
2011-11-11 21:17:41 +0100

    t13 [done] : add class Message in file Message.py

---
+++ Message.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+from collections import namedtuple
+
+class Message(namedtuple(('sender_ID', 'receiver_ID', 'test'))):
+    """
+    Attributes 'sender_ID' and 'receiver_ID' might be equal to zero. It means that
+    the sender/receiver is game system.
+
+    """
+
+    __slots__ = ()
