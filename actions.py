9348d60cb01aa93b512666a8abdd15d16361704b
Krzysztof Medrela <krzysiumed@gmail.com>
2011-11-13 17:31:25 +0100

    t26 [done] : add classes *Action

---
+++ actions.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+"""
+Following actions are avaiable:
+StopAction
+MoveAction
+GatherAction
+StoreAction
+FireAction
+BuildAction
+
+"""
+
+from collections import namedtuple
+
+
+class StopAction(namedtuple('StopAction', ())):
+    __slots__ = ()
+
+class MoveAction(namedtuple('MoveAction', ('source',))):
+    __slots__ = ()
+
+class GatherAction(namedtuple('GatherAction', ('destination',))):
+    __slots__ = ()
+
+class StoreAction(namedtuple('StoreAction', ('storage_ID',))):
+    __slots__ = ()
+
+class FireAction(namedtuple('FireAction', ('destination',))):
+    __slots__ = ()
+
+class BuildAction(namedtuple('BuildAction', ('unit_type', 'destination'))):
+    __slots__ = ()
