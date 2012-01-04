61450386b7e3b54f2562d2748a61397180960937
Krzysztof Medrela <krzysiumed@gmail.com>
2012-01-04 14:01:46 +0100

    improved scriptcraft.core.Environment

--- scriptcraft/core/Environment.py
+++ scriptcraft/core/Environment.py
@@ -14,35 +14,31 @@ class Environment(object):
     def __init__(self, main_folder):
         self.main_folder = main_folder
 
-    def exists_file(self, path):
-        path = self._get_path(path)
+    def exists_file(self, dirty_path):
+        iterable_path, path = self._cleaned_path(dirty_path)
         return os.path.exists(path) and os.path.isfile(path)
 
     def create_file(self, path, data):
-        path_as_iterable = self._get_path_as_iterable(path)
-        path_without_filename = path_as_iterable[:-1]
-        self._create_folder_if_necessary(path_without_filename)
-        path = os.path.join(*path_as_iterable)
+        iterable_path, path = self._cleaned_path(path)
+        self._create_folder_if_necessary_for_file(iterable_path)
         with open(path, 'w') as s:
             s.write(data)
 
-    def remove_folder_recursively(self, folder_path):
-        path = self._get_path(folder_path)
+    def remove_folder_recursively(self, dirty_path):
+        iterable_path, path = self._cleaned_path(dirty_path)
         if os.path.exists(path):
             shutil.rmtree(path)
 
     def copy_file(self, source_path, destination_path):
-        source_path = self._get_path(source_path)
-        destination_path_as_iterable = self._get_path_as_iterable(destination_path)
-        destination_path_without_filename = destination_path_as_iterable[:-1]
-        self._create_folder_if_necessary(destination_path_without_filename)
-        destination_path = os.path.join(*destination_path_as_iterable)
+        iterable_source_path, source_path = self._cleaned_path(source_path)
+        iterable_destination_path, destination_path = self._cleaned_path(destination_path)
+        self._create_folder_if_necessary_for_file(iterable_destination_path)
         if os.path.exists(destination_path):
             raise IOError("File with destination name exists")
         shutil.copyfile(source_path, destination_path)
 
-    def execute_bash_command(self, command, input_data, folder):
-        folder = self._get_path(folder)
+    def execute_bash_command(self, command, input_data, dirty_folder_path):
+        iterable_folder_path, folder = self._cleaned_path(dirty_folder_path)
         process = subprocess.Popen(command,
                                    stdin=subprocess.PIPE,
                                    stdout=subprocess.PIPE,
@@ -53,19 +49,17 @@ class Environment(object):
         exit_code = process.wait()
         return output, errors_output, exit_code
 
-    def _get_path(self, path):
-        path = self._get_path_as_iterable(path)
-        return os.path.join(*path)
+    def _cleaned_path(self, dirty_path):
+        if isinstance(dirty_path, basestring):
+            dirty_path = (dirty_path,)
+        path_as_iterable = (self.main_folder,) + tuple(dirty_path)
+        path = os.path.join(*path_as_iterable)
+        return path_as_iterable, path
 
-    def _get_path_as_iterable(self, path):
-        if isinstance(path, basestring):
-            path = (path,)
-        path = (self.main_folder,) + tuple(path)
-        return path
-
-    def _create_folder_if_necessary(self, folder_path):
+    def _create_folder_if_necessary_for_file(self, path_with_filename):
+        path_without_filename = path_with_filename[:-1]
         full_path = []
-        for folder in folder_path:
+        for folder in path_without_filename:
             full_path.append(folder)
             path = os.path.join(*full_path)
             self._create_folder_if_necessary_for_path(path)
