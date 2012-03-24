f5c5eab755c397c0a7d386ee6b6738e0d34699be
Krzysztof Medrela <krzysiumed@gmail.com>
2012-03-24 17:44:49 +0100

    working on client

--- scriptcraft/client/__init__.py
+++ scriptcraft/client/__init__.py
@@ -5,7 +5,13 @@ import pygtk
 pygtk.require('2.0')
 import gtk
 import math
+import time
+import re
+import os
 
+from scriptcraft.core.Game import Game
+from scriptcraft.core.GameMap import GameMap
+from tests.core.Game import BaseGameTestCase
 from scriptcraft.utils import *
 
 
@@ -23,52 +29,62 @@ class GameViewer(gtk.DrawingArea):
                         gtk.gdk.POINTER_MOTION_MASK)
         self.show()
 
-        self.zoom = 0.05
+        self.zoom = 0.25
         self.delta = [-0.25, -0.25]
 
         self.scaled_sprite_cache = {}
 
+        # set game
+        b = BaseGameTestCase(methodName='__init__')
+        b.setUp()
+        self.game = b.game
+
+
     def expose_callback(self, area, event):
-        self.context = self.window.cairo_create()
-        self.context.rectangle(event.area.x, event.area.y,
-                               event.area.width, event.area.height)
-        self.context.clip()
+        context = self.window.cairo_create()
 
-        self.my_draw(self.context)
-        return False
+        width, height = self.allocation.width, self.allocation.height
+        x_min = max(int(math.floor(self.to_game_coordinate((0, 0))[0])), 0)
+        x_max = min(int(math.ceil(self.to_game_coordinate((width, height))[0])), self.game.game_map.size[0]-1)
+        y_min = max(int(math.floor(self.to_game_coordinate((width, 0))[1])), 0)
+        y_max = min(int(math.ceil(self.to_game_coordinate((0, height))[1])), self.game.game_map.size[1]-1)
 
-        style = self.get_style()
-        gc = style.fg_gc[gtk.STATE_NORMAL]
-
-        self.pixbuf = self.get_scaled_sprite('flat_field', self.zoom)
-        self.pixbuf2 = self.get_scaled_sprite('minerals', self.zoom)
-
-        """
-        gtk.gdk.INTERP_NEAREST    Nearest neighbor sampling; this is the fastest and lowest quality mode. Quality is normally unacceptable when scaling down, but may be OK when scaling up.
-        gtk.gdk.INTERP_TILES    This is an accurate simulation of the PostScript image operator without any interpolation enabled. Each pixel is rendered as a tiny parallelogram of solid color, the edges of which are implemented with antialiasing. It resembles nearest neighbor for enlargement, and bilinear for reduction.
-        gtk.gdk.INTERP_BILINEAR    Best quality/speed balance; use this mode by default. Bilinear interpolation. For enlargement, it is equivalent to point-sampling the ideal bilinear-interpolated image. For reduction, it is equivalent to laying down small tiles and integrating over the coverage area.
-        gtk.gdk.INTERP_HYPER
-        """
-
-        def draw((x, y), pixbuf):
+        def draw_pixbuf((x, y), pixbuf):
             X, Y = self.to_screen_coordinate((x, y))
             X -= 128*self.zoom
             Y -= 144*self.zoom
-            rect = self.allocation
-            if (X <= rect.width and Y <= rect.height and
-                X > -pixbuf.get_width() and Y > -pixbuf.get_height()):
-                self.window.draw_pixbuf(gc, self.pixbuf, 0, 0, X, Y)
-                self.window.draw_pixbuf(gc, self.pixbuf2, 0, 0, X, Y)
-
-        width, height = self.allocation.width, self.allocation.height
-        x_min = int(math.floor(self.to_game_coordinate((0, 0))[0]))
-        x_max = int(math.ceil(self.to_game_coordinate((width, height))[0]))
-        y_min = int(math.floor(self.to_game_coordinate((width, 0))[1]))
-        y_max = int(math.ceil(self.to_game_coordinate((0, height))[1]))
+            context.set_source_pixbuf(pixbuf, X, Y)
+            context.paint()
 
         for x in xrange(x_min, x_max+1):
             for y in xrange(y_min, y_max+1):
-                draw((x, y), self.pixbuf)
+                field = self.game.game_map.get_field((x, y))
+
+                # ground
+                draw_pixbuf((x, y), self.get_scaled_sprite('flat_field'))
+
+                # minerals
+                if field.has_mineral_deposit():
+                    draw_pixbuf((x, y), self.get_scaled_sprite('minerals'))
+
+                # trees
+                if field.has_trees():
+                    draw_pixbuf((x, y), self.get_scaled_sprite('tree'))
+
+                # unit
+                if field.has_unit():
+                    unit = self.game.units_by_IDs[field.get_unit_ID()]
+                    type_name = unit.type.main_name
+                    print unit, type_name
+                    if type_name == '4': # base
+                        draw_pixbuf((x, y), self.get_scaled_sprite('base'))
+                    elif type_name == '6': # tank
+                        draw_pixbuf((x, y), self.get_scaled_sprite('tank'))
+                    elif type_name == '5': # miner
+                        if unit.minerals:
+                            draw_pixbuf((x, y), self.get_scaled_sprite('full_miner'))
+                        else:
+                            draw_pixbuf((x, y), self.get_scaled_sprite('empty_miner'))
 
         return True
 
@@ -112,20 +128,27 @@ class GameViewer(gtk.DrawingArea):
         pixbuf = gtk.gdk.pixbuf_new_from_file("../graphic/%s.png" % name)
         return pixbuf
 
-    def get_scaled_sprite(self, name, scale):
+    def get_scaled_sprite(self, name):
         # if cached, return cached value
-        id = (name, scale)
+        id = (name, self.zoom)
         sprite = self.scaled_sprite_cache.get(id, None)
         if sprite:
             return sprite
 
         # otherwise compute, save in cache and return
         sprite = self.get_sprite(name)
-        new_width = int(sprite.get_width()*scale)+2
-        new_height = int(sprite.get_height()*scale)+2
+        new_width = int(sprite.get_width()*self.zoom)+2
+        new_height = int(sprite.get_height()*self.zoom)+2
         scaled = sprite.scale_simple(new_width, new_height, gtk.gdk.INTERP_BILINEAR)
         self.scaled_sprite_cache[id] = scaled
         return scaled
+
+        """
+        gtk.gdk.INTERP_NEAREST    Nearest neighbor sampling; this is the fastest and lowest quality mode. Quality is normally unacceptable when scaling down, but may be OK when scaling up.
+        gtk.gdk.INTERP_TILES    This is an accurate simulation of the PostScript image operator without any interpolation enabled. Each pixel is rendered as a tiny parallelogram of solid color, the edges of which are implemented with antialiasing. It resembles nearest neighbor for enlargement, and bilinear for reduction.
+        gtk.gdk.INTERP_BILINEAR    Best quality/speed balance; use this mode by default. Bilinear interpolation. For enlargement, it is equivalent to point-sampling the ideal bilinear-interpolated image. For reduction, it is equivalent to laying down small tiles and integrating over the coverage area.
+        gtk.gdk.INTERP_HYPER
+        """
 
 
 class ClientWindow(gtk.Window):
@@ -137,11 +160,30 @@ class ClientWindow(gtk.Window):
 
 class HelloWorld(object):
     def __init__(self):
+        # build GUI
         self.area = GameViewer()
         self.area.show()
         self.window = ClientWindow()
         self.window.add(self.area)
+        self._build_menu_bar()
         self.window.show()
+
+    def _build_menu_bar(self):
+        # create menu
+        items = [gtk.MenuItem("New game"),
+                 gtk.MenuItem("Open game"),
+                 gtk.MenuItem("Save game"),
+                 gtk.MenuItem("Add player"),
+                 gtk.MenuItem("Tic"),
+                 gtk.MenuItem("Quit")]
+        menu_game = gtk.Menu()
+        map(menu_game.append, items)
+        map(lambda i: i.show(), items)
+
+        # create menubar
+        menubar = gtk.MenuBar()
+        self.window.add(menubar)
+        menubar.show()
 
     def main(self):
         gtk.main()
