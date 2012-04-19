5c2b235807551061cda64249641d636040abc144
Krzysztof Medrela <krzysiumed@gmail.com>
2012-04-19 20:44:29 +0200

    added upload_to_pypi.sh script to automatize uploading source and windows distribution to pypi

---
+++ scripts/upload_to_pypi.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+printf "COPYING... "
+
+dir=`dirname "$0"`
+cd "$dir" # so we are in the same directory where this script is in
+cd ../.. # so we are in parent of scriptcraft root directory
+
+TMP=~/tmp
+cp -f -r scriptcraft "$TMP"
+cd "$TMP/scriptcraft"
+
+printf "OK\n"
+
+python setup.py register sdist upload
+# wine C:\\Python25\\python.exe setup.py register bdist_wininst --target-version=2.5 upload
+wine C:\\Python26\\python.exe setup.py register bdist_wininst --target-version=2.6 upload
+wine C:\\Python27\\python.exe setup.py register bdist_wininst --target-version=2.7 upload
