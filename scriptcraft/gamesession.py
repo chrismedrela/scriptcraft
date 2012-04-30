0a4f0dedc2a6d8b7bcf482b88321668f85537c87
Krzysztof Medrela <krzysiumed@gmail.com>
2012-04-30 18:20:35 +0200

    added gamesession.LanguageConfiguration class; changed gamesession.{SystemConfiguration, GameSession}

--- scriptcraft/gamesession.py
+++ scriptcraft/gamesession.py
@@ -12,8 +12,28 @@ from scriptcraft.core.compileAndRunProgram import CompileAndRunProgram
 from scriptcraft.core.Language import Language
 
 
+
+class LanguageConfiguration(object):
+    def __init__(self, source_file_name, binary_file_name,
+                 compilation_command, running_command):
+        self.source_file_name = source_file_name
+        self.binary_file_name = binary_file_name
+        self.compilation_command = compilation_command
+        self.running_command = running_command
+
+
 class SystemConfiguration(object):
-    pass
+    def __init__(self):
+        self.languages_configurations = {
+            Language.PYTHON : LanguageConfiguration(
+                'src.py', 'bin.py',
+                'cp src.py bin.py',
+                'python bin.py'),
+            Language.CPP : LanguageConfiguration(
+                'src.cpp', 'bin',
+                'g++ src.cpp -o bin',
+                './bin')
+        }
 
 
 class GameSession(object):
@@ -33,17 +53,17 @@ class GameSession(object):
         pickle.dump(self.game, open(self._game_file, 'w'))
 
     def tic(self):
+        languages = self._system_configuration.languages_configurations.items()
+        source_file_names = dict([(k, v.source_file_name) for k, v in languages])
+        binary_file_names = dict([(k, v.binary_file_name) for k, v in languages])
+        compilation_commands = dict([(k, v.compilation_command) for i in languages])
+        running_commands = dict([(k, v.running_command) for i in languages])
         compile_and_run = CompileAndRunProgram(
             self._directory,
-            {Language.CPP:'src.cpp',
-             Language.PYTHON:'src.py'},
-            {Language.CPP:'bin.exe',
-             Language.PYTHON:'bin.py'},
-            {Language.CPP:'g++ src.cpp -o bin.exe',
-             Language.PYTHON:'cp src.py bin.py'},
-            {Language.CPP:'./bin.exe',
-             Language.PYTHON:'python bin.py'}
-        )
+            source_file_names,
+            binary_file_names,
+            compilation_commands,
+            running_commands)
         self.game.tic(compile_and_run)
 
     def __getattr__(self, name):
