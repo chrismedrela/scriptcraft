cff21945da1fc10498c1e6c9eda6d71c38f1716c
Krzysztof Medrela <krzysiumed@gmail.com>
2012-06-04 15:26:39 +0200

    fixed bug: generating windows distribute .zip file in upload_to_pypi.sh script

--- scripts/upload_to_pypi.sh
+++ scripts/upload_to_pypi.sh
@@ -16,5 +16,10 @@ printf "OK\n"
 python setup.py register sdist upload
 
 # create distributions for windows
-wine C:\\Python27\\python.exe setup.py py2exe --target-version=2.7
-zip -r scriptcraft-for-windows.zip dist/py2exe
+wine C:\\Python27\\python.exe setup.py py2exe
+cd dist/py2exe
+zip -r zipus.zip .
+mv zipus.zip ../../scriptcraft-for-windows.zip
+cd ../..
+
+printf ">>> Now commit scriptcraft-for-windows.zip. <<<\n"
