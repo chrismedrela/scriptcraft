b88625241230310d5a99c9488a4c9ccc2b667d94
Krzysztof Medrela <krzysiumed@gmail.com>
2012-09-08 14:40:19 +0200

    changed and renamed build script

---
+++ scripts/build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# change current directory
+dir=`dirname "$0"`
+cd "$dir"
+cd .. # so we are in scriptcraft root directory
+
+pwd=`pwd`
+printf "Changed current directory to: ${pwd}\n"
+
+rm -r dist
+rm -r build
+rm `find . | grep ".pyc$"`
+
+# create distributions for windows
+wine C:\\Python27\\python.exe setup.py py2exe
+cd dist/py2exe
+zip -r zipus.zip .
+mv zipus.zip ../../scriptcraft-for-windows.zip
+cd ../..
+
+printf ">>> Now commit scriptcraft-for-windows.zip. <<<\n"
