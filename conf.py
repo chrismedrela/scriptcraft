183e59640c04e625fae7003ff5f19d1ea08a6cd3
Krzysztof Medrela <krzysiumed@gmail.com>
2012-09-11 21:31:54 +0200

    added script for rendering README to html file

---
+++ conf.py
@@ -0,0 +1,13 @@
+master_doc = 'README'
+language = 'pl'
+html_theme = 'default'
+html_title = "Scriptcraft"
+html_short_title = "Scriptcraft"
+html_show_copyright = False
+html_show_sphinx = False
+html_show_sourcelink = False
+html_use_index = False
+html_sidebars = {
+    '**' : [],
+}
+html_add_permalinks = None
---
+++ scripts/build_html.sh
@@ -0,0 +1,5 @@
+sphinx-build . . README.rst
+rm search.html
+rm searchindex.js
+rm objects.inv
+rm genindex.html
