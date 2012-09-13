22b8ee5bce18d7f68517c7cbfd2b399e6fe38bf6
Krzysztof Medrela <krzysiumed@gmail.com>
2012-09-13 10:17:35 +0200

    made build.sh more verbose

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,25 +6,31 @@ cd "$dir"
 cd .. # so we are in scriptcraft root directory
 
 pwd=`pwd`
-printf "Changed current directory to: ${pwd}\n"
+printf "\nCHANGED CURRENY DIRECTORY TO: ${pwd}\n"
 
 # make README.html
+printf '\nRENDERING README.html\n'
 . scripts/build_html.sh
 
 # clean up before creating distributions
+printf '\nCLEANING BEFORE CREATING DISTRIBUTIONS\n'
 rm -r dist
 rm -r build
 rm `find . | grep ".pyc$"`
 
 # create distribution for windows
-wine C:\\Python27\\python.exe setup.py py2exe
+printf '\nCREATING WINDOWS DISTRIBUTION\n'
+wine C:\\Python27\\python.exe setup.py py2exe > .buildlog_windist
 cd dist/py2exe
 mkdir games # create empty games directory
-zip -r zipus.zip .
+printf '\nZIPPING WINDOWS DISTRIBUTION\n'
+zip -r zipus.zip . > ../../.buildlog_zipping_windist
 mv zipus.zip ../../scriptcraft-for-windows.zip
 cd ../..
 
-printf ">>> Now commit scriptcraft-for-windows.zip. <<<\n"
-
 # clean up
+printf '\nCLEANING UP\n'
 rm README.html
+
+# finish
+printf "\nNOW COMMIT scriptcraft-for-windows.zip.\n"
